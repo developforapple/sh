4b99abfb15092f89a834bcc7634cfa77d8c7f238
tiny <developforapple@163.com>
2017-11-29 17:57:54 +0800

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -2,9 +2,3 @@
 
 name="com.mobilepower.terminal"
 echo $name
-# PROCESS=`busybox ps -ef | grep name | grep -v grep | grep -v PPID | busybox awk '{ print $1}'`
-# for i in $PROCESS
-# do
-#   echo "Kill the ${name} process [ $i ]"
-#   busybox kill -9 $i
-# done
